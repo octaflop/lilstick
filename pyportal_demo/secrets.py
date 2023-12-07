fa77044005c9a7895a9bab0be7ea8ebbf33d43b2
Faris Chebib <faris@theluckybead.com>
2023-12-06 18:39:37 -0700

    Add pyportal demo

---
+++ pyportal_demo/secrets.py
@@ -0,0 +1,10 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'ssid',
+    'password' : 'hi',
+    'timezone' : "America/Denver", # http://worldtimeapi.org/timezones
+    'github_token' : '',
+    'hackaday_token' : '',
+    }
